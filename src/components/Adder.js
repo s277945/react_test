cd12cf3bc6dc24dcbddfc494b27ef872b543e29d
s277945 <s277945@studenti.polito.it>
2023-09-14 17:43:16 +0200

    Removed redundant code

--- src/components/Adder.js
+++ src/components/Adder.js
@@ -4,20 +4,11 @@ import bootstrap from 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/+esm'
 
 export default function Adder() {
     const [rows, setRows] = useState([]);
-    const enableRow = (index) => {
+    const setStateRow = (state, index) => {
         let newRows = rows;
-        if (newRows[index] && !newRows[index].enabled) {
+        if (newRows[index] && newRows[index].enabled != state) {
             let oldRow = newRows[index];
-            let newRow = {sign: oldRow.sign, value: oldRow.value, enabled: true}
-            newRows[index] = newRow;
-            setRows([...newRows]);
-        }
-    };
-    const disableRow = (index) => {
-        let newRows = rows;
-        if (newRows[index] && newRows[index].enabled) {
-            let oldRow = newRows[index];
-            let newRow = {sign: oldRow.sign, value: oldRow.value, enabled: false}
+            let newRow = {sign: oldRow.sign, value: oldRow.value, enabled: state}
             newRows[index] = newRow;
             setRows([...newRows]);
         }
@@ -52,6 +43,7 @@ export default function Adder() {
             <div>
                 <button onClick={() => setRows(rows => [...rows, {sign: "+", value: 0, enabled: true}])}>Add row</button>
             </div>
+
             <ul>
                {rows.map((row, index) => 
                 <li key={index}>
@@ -62,11 +54,12 @@ export default function Adder() {
                         <input type="text" value={row.value} onChange={(event) => changeRowValue(event.target.value, index)}/>
                         <button onClick={() => deleteRow(index)}> Delete </button>
                         {row.enabled ? 
-                        <button onClick={() => disableRow(index)}> Disable </button> : 
-                        <button onClick={() => enableRow(index)}> Enable </button>}
+                        <button onClick={() => setStateRow(false, index)}> Disable </button> : 
+                        <button onClick={() => setStateRow(true, index)}> Enable </button>}
                     </li>
                 )} 
             </ul>
+
             <div>
                 Result: {rows.reduce((tot, curr) => 
                             tot + parseInt(curr.value && curr.enabled ?
