6bd880054de879be824363b78d7078393988f592
s277945 <s277945@studenti.polito.it>
2023-09-19 00:58:46 +0200

    Added HashRouter to run react app without server

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import Adder from './components/Adder';
+import { HashRouter } from 'react-router-dom' ;
 
 function App() {
   return (
-    <div className="page-wrap">
-      
-      {/* Challenge Title */}
-      <h1>Simple React calculator</h1>
+    <HashRouter>
+      <div className="page-wrap">
+        
+        {/* Challenge Title */}
+        <h1>Simple React calculator</h1>
 
-      {/* Adder component */}
-      <Adder/>
+        {/* Adder component */}
+        <Adder/>
 
-    </div>
+      </div>
+    </HashRouter>
   );
 }
 
